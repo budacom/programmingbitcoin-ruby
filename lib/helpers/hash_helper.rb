53f943838e53cae07e5bb05ff8e7498015b11778
Javier G. Montoya S <jgmontoya@pm.me>
2022-11-24 16:23:39 -0300

    feat(helpers/hash_helper): add murmur3 implementation

--- lib/helpers/hash_helper.rb
+++ lib/helpers/hash_helper.rb
@@ -1,6 +1,8 @@
 require 'digest'
 
 module HashHelper
+  MASK32 = 0xFFFFFFFF
+
   def self.hash160(string)
     first_round = Digest::SHA256.digest(string)
     Digest::RMD160.digest(first_round)
@@ -10,4 +12,57 @@ module HashHelper
     first_round = Digest::SHA256.digest(string)
     Digest::SHA256.digest(first_round)
   end
+
+  # rubocop:disable Metrics/MethodLength
+  def self.murmur3(string, seed: 0) # rubocop:disable Metrics/AbcSize
+    key_bytes = string.bytes
+    result_hash = seed
+
+    rounded_end = (key_bytes.length & 0xfffffffc)
+    (0...rounded_end).step(4) do |i|
+      aux = block32(key_bytes, i)
+      result_hash ^= scramble32(aux)
+      result_hash = rotl32(result_hash, 13)
+      result_hash = result_hash * 5 + 0xe6546b64
+    end
+
+    val = key_bytes.length & 3
+
+    aux = 0
+    (0...3).reverse_each do |i|
+      aux |= (key_bytes[rounded_end + i] & 0xff) << (8 * i) if val >= (i + 1)
+    end
+
+    result_hash ^= scramble32(aux)
+    finalization_mix(result_hash ^ key_bytes.length)
+  end
+  # rubocop:enable Metrics/MethodLength
+
+  private
+
+  def self.finalization_mix(result_hash)
+    result_hash ^= ((result_hash & MASK32) >> 16)
+    result_hash *= 0x85ebca6b
+    result_hash ^= ((result_hash & MASK32) >> 13)
+    result_hash *= 0xc2b2ae35
+    (result_hash ^ ((result_hash & MASK32) >> 16)) & MASK32
+  end
+
+  def self.block32(key_bytes, index)
+    (1..3).map do |i|
+      key_bytes[index + i] << (8 * i)
+    end.reduce(key_bytes[index], :|)
+  end
+
+  def self.rotl32(item, bits_to_rotate)
+    ((item << bits_to_rotate) | ((item & MASK32) >> (32 - bits_to_rotate))) & MASK32
+  end
+
+  def self.scramble32(aux)
+    aux = (aux * 0xcc9e2d51) & MASK32
+    aux = rotl32(aux, 15)
+    (aux * 0x1b873593) & MASK32
+  end
+
+  private_class_method :finalization_mix, :block32, :rotl32, :scramble32
 end
--- spec/helpers/hash_helper_spec.rb
+++ spec/helpers/hash_helper_spec.rb
@@ -1,17 +1,29 @@
 require 'helpers/hash_helper'
 
 RSpec.describe HashHelper do
-  describe '#hash160' do
+  describe '.hash160' do
     it 'computes the hash by sha256 followed by ripemd160' do
       hash = '77bc43ce98ed7de19a42b6f2b8978df300890a2d'
       expect(described_class.hash160('Bitcoin Guild rocks!').unpack1('H*')).to eq(hash)
     end
   end
 
-  describe '#hash256' do
+  describe '.hash256' do
     it 'computes the hash by two passes of sha256' do
       hash = 'a63898c9855b802d6db18886928affbc22b928c3ccd683e21d62da8f0af00a42'
       expect(described_class.hash256('Bitcoin Guild rocks!').unpack1('H*')).to eq(hash)
+    end
+  end
+
+  describe '.murmur3' do
+    it 'computes the correct murmur3 hash for different message and seeds' do
+      [1203516251, 669393163, 819509628, 3765971536].each_with_index do |expected_hash, seed|
+        expect(described_class.murmur3('Bitcoin Guild rocks!', seed: seed)).to eq(expected_hash)
+      end
+      expect(described_class.murmur3('Goodbye!', seed: 8443760525)).to eq(468028502)
+      [1411415842, 2371772749, 4164319582, 2164673664].each_with_index do |expected_hash, seed|
+        expect(described_class.murmur3("Bitcoin Guild rocks! #{seed}")).to eq(expected_hash)
+      end
     end
   end
 end
